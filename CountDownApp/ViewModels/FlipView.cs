0f6935a16caaef63bc9bcd23e77efba0700fdabd
manikandan280396 <162673683+manikandan280396@users.noreply.github.com>
2024-03-08 12:50:13 +0530

    Add files via upload

---
+++ CountDownApp/ViewModels/FlipView.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CountDownApp.ViewModels
+{
+	public class FlipView : ContentView
+    {
+        private Label topLabel;
+        private Label bottomLabel;
+        private BoxView flipBox;
+
+        public FlipView()
+        {
+            topLabel = new Label
+            {
+
+                Padding = 10,
+                VerticalOptions = LayoutOptions.Fill,
+                HorizontalOptions = LayoutOptions.Fill,
+                FontSize = 24,
+                FontAttributes = FontAttributes.Bold,
+                TextColor = Color.FromRgb(0, 0, 0),
+                WidthRequest = 55,
+                HeightRequest=55,
+                BackgroundColor = Color.FromRgb(255, 255, 255)
+            };
+
+            bottomLabel = new Label
+            {
+                Padding = 10,
+                FontAttributes = FontAttributes.Bold,
+                VerticalOptions = LayoutOptions.Fill,
+                HorizontalOptions = LayoutOptions.Fill,
+                FontSize = 24,
+                WidthRequest = 55,
+                HeightRequest = 55,
+                TextColor = Color.FromRgba(252, 96, 135, 255),
+                BackgroundColor = Color.FromRgba(52, 55, 80, 255)
+
+            };
+
+            flipBox = new BoxView
+            {
+                VerticalOptions = LayoutOptions.FillAndExpand,
+                HorizontalOptions = LayoutOptions.FillAndExpand,
+                Color = Color.FromRgba(255, 0, 0, 0)
+            };
+
+            var flipGesture = new TapGestureRecognizer();
+            flipGesture.Tapped += OnFlipTapped;
+
+            GestureRecognizers.Add(flipGesture);
+
+            Content = new Grid
+            {
+                Children = { topLabel, bottomLabel, flipBox }
+            };
+        }
+
+        private async void OnFlipTapped(object sender, EventArgs e)
+        {
+            await this.RotateXTo(180, 250, Easing.Linear);
+            (topLabel.Text, bottomLabel.Text) = (bottomLabel.Text, topLabel.Text);
+            await this.RotateXTo(0, 250, Easing.Linear);
+        }
+
+        public async Task SetNumberAsync(int number)
+        {
+            if (topLabel.Text != number.ToString())
+            {
+                topLabel.Text = number.ToString();
+                await FlipAsync();
+            }
+        }
+
+        private async Task FlipAsync()
+        {
+            await this.RotateXTo(90, 250, Easing.Linear);
+            (topLabel.Text, bottomLabel.Text) = (bottomLabel.Text, topLabel.Text);
+            await this.RotateXTo(0, 250, Easing.Linear);
+        }
+
+
+    }
+
+}
+
---
+++ CountDownApp/Views/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace CountDownApp
+{
+    public partial class MainPage : ContentPage
+    {
+        private TimeSpan totalTime;
+        private bool isTimerRunning = false;
+        public MainPage()
+        {
+            InitializeComponent();
+            InitializeFlipViews();
+            if (!isTimerRunning)
+            {
+                Device.StartTimer(TimeSpan.FromSeconds(1), () =>
+                {
+                    UpdateTimer();
+                    return isTimerRunning && totalTime.TotalSeconds > 0;
+                });
+
+                isTimerRunning = true;
+            }
+
+        }
+        private void InitializeFlipViews()
+        {
+            totalTime = TimeSpan.FromDays(8) + TimeSpan.FromHours(23) + TimeSpan.FromMinutes(55) + TimeSpan.FromSeconds(45);
+
+            dayFlipView.SetNumberAsync(totalTime.Days);
+            hourFlipView.SetNumberAsync(totalTime.Hours);
+            minuteFlipView.SetNumberAsync(totalTime.Minutes);
+            secondFlipView.SetNumberAsync(totalTime.Seconds);
+        }
+
+        
+
+        private void UpdateTimer()
+        {
+            totalTime = totalTime.Subtract(TimeSpan.FromSeconds(1));
+
+            if (totalTime.TotalSeconds < 0)
+            {
+                totalTime = TimeSpan.Zero;
+                isTimerRunning = false;
+            }
+
+            dayFlipView.SetNumberAsync(totalTime.Days);
+            hourFlipView.SetNumberAsync(totalTime.Hours);
+            minuteFlipView.SetNumberAsync(totalTime.Minutes);
+            secondFlipView.SetNumberAsync(totalTime.Seconds);
+        }
+
+        void tapImage_Tapped(System.Object sender, System.EventArgs e)
+        {
+             Launcher.OpenAsync("http://facebook.com");
+        }
+
+        void tapImage_Tapped_1(System.Object sender, System.EventArgs e)
+        {
+            Launcher.OpenAsync("http://facebook.com");
+        }
+
+        void tapImage_Tapped_2(System.Object sender, System.EventArgs e)
+        {
+            Launcher.OpenAsync("http://instagram.com");
+        }
+    }
+}
+
+
